abb490d0b74137c357a35616af080a90922470cc
tomaszs86 <tomaszstolarczyk86@gmail.com>
2025-09-29 11:55:14 +0200

    Update streamlit_app.py
    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ ingredients_list = st.multiselect(
 )
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 if ingredients_list:
     ingredients_string = " ".join(ingredients_list)
