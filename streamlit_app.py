2403e86dfa659fc2438d386a6051382f1aa5b1ef
tomaszs86 <tomaszstolarczyk86@gmail.com>
2025-09-29 14:43:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,25 @@ ingredients_list = st.multiselect(
     options=fruit_options,
     max_selections=5
 )
+
+name_on_order = st.text_input("Your name on the order:")
+
+if ingredients_list:
+    for fruit_chosen in ingredients_list:
+        st.subheader(f"{fruit_chosen} Nutrition Information")
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+    ingredients_string = ' '.join(ingredients_list)
+    orders_df = session.create_dataframe(
+        [[ingredients_string, name_on_order]],
+        schema=["ingredients", "name_on_order"]
+    )
+
+    st.write("Order preview:")
+    st.write(orders_df.to_pandas())
+
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
+        orders_df.write.mode("append").save_as_table("smoothies.public.orders")
+        st.success('Your Smoothie is ordered!', icon="✅")
